ee354af60bfb076b75f4396f73ba578b7de13598
ydozen <ydozen@DozennoMacBook-ea.local>
2018-11-14 17:32:08 +0900

    動作確認(仮)

--- tutorial/polls/api_urls.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from . import api_views
-
-question_router = DefaultRouter()
-question_router.reqister(r'', api_views.QuestionViewSet)
--- tutorial/polls/api_view.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
-
-from .models import Question
-from .serializers import QuestionSerializer
-
-class QuestionViewSet(ReadOnlyModelViewSet):
-    queryset = Question.objects.all() # ここが対象となるレコード指定．今回は全部
-    serializer_class = QuestionSerializer # 戻り値を定義したSerializer
-    
